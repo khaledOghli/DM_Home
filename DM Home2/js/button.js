88d49c4286f3abd32dac3408bb32ed786ff673ca
khaledOghli <65956317+khaledOghli@users.noreply.github.com>
2020-10-14 14:34:50 +0400

    Add files via upload

---
+++ DM Home2/js/button.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $('[ripple]').on('click', function(e) {
+    var rippleDiv = $('<div class="ripple" />'),
+      rippleOffset = $(this).offset(),
+      rippleY = e.pageY - rippleOffset.top,
+      rippleX = e.pageX - rippleOffset.left,
+      ripple = $('.ripple');
+
+    rippleDiv.css({
+      top: rippleY - (ripple.height() / 2),
+      left: rippleX - (ripple.width() / 2),
+      background: $(this).attr("ripple-color")
+    }).appendTo($(this));
+
+    window.setTimeout(function() {
+      rippleDiv.remove();
+    }, 1500);
+  });
+});
